2dfc1f3cdc2a493aef35f55e6a07a5487791bf1d
flyboy <jay@up1987.com>
2016-07-12 20:58:35 +0800

    add

--- backup.sh
+++ backup.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+BACKUP_DIR=/backup
+SOURCE_DIR=/home/shiyanlou
+D_TODAY=${DT:-$(date +%Y%m%d)}
+D_YESTERDAY=$(date -d "$D_TODAY -1day" +%Y%m%d )
 
+if [ -d "$BACKUP_DIR/$D_YESTERDAY" ] ; then
+    sudo rsync -a --link-dest=$BACKUP_DIR/$D_YESTERDAY $SOURCE_DIR $BACKUP_DIR/$D_TODAY
+else
+    sudo rsync -a $SOURCE_DIR $BACKUP_DIR/$D_TODAY
+fi
+
+result=$?
+
+if [ $result -eq 0 ] ; then
+    echo "backup done."
+else
+    echo "backup failed."
+fi
